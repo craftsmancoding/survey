b46a8e6b45ade56b973c900a0a08071c418cc1e1
Everett Griffiths <everett@fireproofsocks.com>
2013-12-20 14:02:41 -0800

    Moving things around for Repoman...

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Configuration details about this package.
+ *
+ * This file is used by Repoman to build and install this package.
+ * https://github.com/craftsmancoding/repoman
+ */
+return array(
+    'package_name' => 'Survey',
+    'package_name_lower' => 'survey',
+    'description' => 'Create and administer surveys and questionnaires with any number of questions.',
+    'version' => '1.0.0',
+    'release' => 'beta',
+    'author_name' => 'Everett Griffiths',
+    'author_email' => 'everett@craftsmancoding.com',
+    'author_site' => 'Craftsman Coding',    
+    'author_url' => 'http://craftsmancoding.com/',
+    'documentation_url' => 'https://github.com/craftsmancoding/survey/wiki',
+    'repo_url' => 'https://github.com/craftsmancoding/survey/',
+    'copyright' => '2013',
+    'docblocks_reqd' => true, // skip the chunks
+);
+/*EOF*/
